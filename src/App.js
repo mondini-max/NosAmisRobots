b485a7f57f29fee91c8ae5eec7307e2b101e9a4d
moshtaghfr <moshtagh.fr@gmail.com>
2021-06-27 22:03:00 +0200

    Create CardList component and refactor the code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React from 'react';
-import Card from './components/card';
+import CardList from './components/CardList';
 import { robots } from './data/robots';
 const App = () => {
   return (
     <div>
-      <Card
-        id={robots[0].id}
-        name={robots[0].name}
-        username={robots[0].username}
-        email={robots[0].email}
-      />
-      <Card
-        id={robots[1].id}
-        name={robots[1].name}
-        username={robots[1].username}
-        email={robots[1].email}
-      />
-      <Card
-        id={robots[2].id}
-        name={robots[2].name}
-        username={robots[2].username}
-        email={robots[2].email}
-      />
+      <CardList robots={robots} />
     </div>
   );
 };
