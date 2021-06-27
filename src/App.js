fdbf15df4d266f2194fd422e8357256d4c0c9266
moshtaghfr <moshtagh.fr@gmail.com>
2021-06-27 23:05:47 +0200

    Add state to App and integrate search function to find a robot

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './components/CardList';
+import Searchbox from './components/Searchbox';
 import { robots } from './data/robots';
-const App = () => {
-  return (
-    <div>
-      <CardList robots={robots} />
-    </div>
-  );
-};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: '',
+    };
+  }
+  onSearchChange = (event) => {
+    let search = event.target.value;
+    this.setState({ searchfield: search });
+  };
+  render() {
+    const searchedRobot = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    // console.log(searchedRobot);
+    return (
+      <div className='tc'>
+        <h1>Robot friends</h1>
+        <Searchbox searchChange={this.onSearchChange} />
+        <CardList robots={searchedRobot} />
+      </div>
+    );
+  }
+}
 
 export default App;
