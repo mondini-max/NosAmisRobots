5bfb4980f1381a5c1783cb55e2df1f226ced87f1
moshtaghfr <moshtagh.fr@gmail.com>
2021-06-28 21:27:12 +0200

    remove data.json form App.js and get data by fetch and inject into app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import CardList from './components/CardList';
 import Searchbox from './components/Searchbox';
-import { robots } from './data/robots';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     };
-    console.log('test performance 1', 'constructor()');
+    // console.log('test performance 1', 'constructor()');
   }
   componentDidMount() {
-    console.log('check lifecycle methods');
-    this.setState({ robots: robots });
-    console.log('test performance 2', 'componentDidMount()');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+    // console.log('check lifecycle methods');
+    // console.log('test performance 2', 'componentDidMount()');
   }
   onSearchChange = (event) => {
     let search = event.target.value;
@@ -28,15 +29,19 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    console.log('test performance 3', 'render()');
+    // console.log('test performance 3', 'render()');
     // console.log(searchedRobot);
-    return (
-      <div className='tc'>
-        <h1 className='f2'>Robot friends</h1>
-        <Searchbox searchChange={this.onSearchChange} />
-        <CardList robots={searchedRobot} />
-      </div>
-    );
+    if (this.state.robots === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f2'>Robot friends</h1>
+          <Searchbox searchChange={this.onSearchChange} />
+          <CardList robots={searchedRobot} />
+        </div>
+      );
+    }
   }
 }
 
