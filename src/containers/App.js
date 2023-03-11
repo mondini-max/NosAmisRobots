628e7293e75338414c3bc973e7268eee93face89
Maxime Mondini <mh-mondini@hotmail.com>
2023-03-11 16:48:04 -0500

    update title and add helmet to bring reactive title

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,12 +5,14 @@ import './App.css';
 import ErrorBoundry from '../components/ErrorBoundry';
 import Scroll from '../components/Scroll';
 import Footer from '../components/Footer';
+import {Helmet} from 'react-helmet'
 
 
 const App = () => {
   const [robots, setRobots] = useState([]);
   const [searchfield, setSearchfield] = useState('');
   // const [count, setCount] = useState(0);
+
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -31,6 +33,11 @@ const App = () => {
     <h1>Loading</h1>
   ) : (
     <div className='tc'>
+    <Helmet>
+                <meta charSet="utf-8" />
+                <title>Nos amis robots</title>
+                <link rel="canonical" href="https://mondini-max.github.io/NosAmisRobots/" />
+      </Helmet>
       <h1 className='f2'>Nos amis robots</h1>
       {/* <button onClick={() => setCount(count + 1)}>click me </button> */}
       <Searchbox searchChange={onSearchChange} />
